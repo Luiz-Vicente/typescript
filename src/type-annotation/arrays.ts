f7dbdaa0f497830ec5020124fd79df92f8a9d472
Luiz Fernando <fernandoluiz43673@gmail.com>
2023-08-24 09:29:55 -0300

    - added new array and tuple examples

--- src/type-annotation/arrays.ts
+++ src/type-annotation/arrays.ts
@@ -1,13 +1,17 @@
 /* ARRAY TYPES */
 const names: string[] = ['Alice', 'Bob', 'Charlie']; // 'names' is declared as an array of strings.
 const ages: number[] = [25, 30, 22]; // 'ages' is declared as an array of numbers.
+const fruits: readonly string[] = ['Orange', 'Pinaple', 'Apple']; // This array cannot be changed.
 // also:
 const ArrayNames: Array<string> = ['Alice', 'Bob', 'Charlie']; // 'names' is declared as an array of strings.
 const ArrayAges: Array<number> = [25, 30, 22]; // 'ages' is declared as an array of numbers.
+const ArrayFruits: ReadonlyArray<string> = ['Orange', 'Pinaple', 'Apple']; // This array cannot be changed.
 
 export default {
   names,
   ages,
+  fruits,
   ArrayNames,
   ArrayAges,
+  ArrayFruits,
 };
--- src/type-annotation/tuple.ts
+++ src/type-annotation/tuple.ts
@@ -1,4 +1,11 @@
 /* TUPLE TYPE */
-const tuple: [number, string, boolean] = [42, 'hello', true]; // 'tuple' is declared as a tuple with specific element types.
+const tuple: [number, string, boolean] = [42, 'hello', true];
+// 'tuple' is declared as a tuple with specific element types.
 
-export default tuple;
+const tupleWithOptionalValue: [string, number, string?] = ['avocado', 2];
+// the last value is optional using this syntax and this is valid for all types.
+
+const myTuple: [string, number, ...string[]] = ['avocado', 2];
+// this tuple now assumes that all elements that are inserted at the end of the Array will be strings
+
+export default { tuple, tupleWithOptionalValue, myTuple };
